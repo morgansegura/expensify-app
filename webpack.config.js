18971c7e15d610fc1d55e63f5133e1fd1286f807
Morgan Segura <morgansegura@gmail.com>
2018-08-24 07:49:33 -0700

    There was a rogue quotation mark in my .env file

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,7 @@ module.exports = (env) => {
                 'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
                 'process.env.FIREBASE_ORIJECT_ID': JSON.stringify(process.env.FIREBASE_ORIJECT_ID),
                 'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-                'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDING_ID)
+                'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDING_ID) 
             })
         ],
         devtool: isProduction ? 'source-map' : 'inline-source-map',
